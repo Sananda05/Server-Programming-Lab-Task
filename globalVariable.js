735e244ad6f28e3ab1f5e19cb840ca40fc4162e4
Sananda <sananda@iut-dhaka.edu>
2021-05-05 17:30:01 +0600

    completed globalvar.js

---
+++ globalVariable.js
@@ -0,0 +1,16 @@
+
+//current directory
+const currentDir = __dirname ;
+console.log(currentDir);
+
+//filename
+
+console.log(__filename);
+
+//require = func to use modules
+
+const fs = require("fs");
+
+//process = info about where the program is executed
+
+console.log(process);
