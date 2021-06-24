8d5523ec759afc065cac7b822ebd28fef21d0158
Sananda <sananda@iut-dhaka.edu>
2021-06-24 17:06:05 +0600

    auth controller added

---
+++ 02.LabTwoAndThree/SimpleAuth/Controller/authcontroller.js
@@ -0,0 +1,93 @@
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const alert = require("alert");
+const jwt = require("jsonwebtoken");
+var LocalStorage = require("node-localstorage").LocalStorage,
+localStorage = new LocalStorage("./scratch");
+
+const User = require("../model/usersModel/user");
+
+JWT_SECRET = "aghddshjhdhebgdshdjkhxsnakjdsjsjdd674773$%$#sshsshhsdhssj";
+
+const getRegister = (req, res) => {
+  res.sendFile("register.html", { root: "./views/authView" });
+};
+
+const postRegister = async (req, res) => {
+  const { username, email, gender, password, Confirm_password } = req.body;
+
+  if (!username || typeof username !== "string") {
+    alert("Inavlid Username");
+    res.redirect("/");
+  }
+  if (!email || typeof email !== "string") {
+    alert("Inavlid email");
+    res.redirect("/");
+  }
+
+  if (password.length >= 6) {
+    if (password === Confirm_password) {
+      const pass = await bcrypt.hash(password, 10);
+
+      try {
+        const newUser = await User.create({
+          username,
+          email,
+          gender,
+          pass,
+        });
+        console.log("User created successfully", newUser);
+        res.redirect("/login");
+      } catch (error) {
+        if (error.code === 11000) {
+          alert("Username or Email already registered");
+        }
+        console.log(error);
+      }
+    } else {
+      alert("Password doesn't match !");
+      res.redirect("/");
+    }
+  } else {
+    alert("Password must be at least 6 digits");
+    res.redirect("/");
+  }
+};
+
+const getLogin = (req, res) => {
+  res.sendFile("login.html", { root: "./views/authView" });
+};
+
+const postLogin = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    if (bcrypt.compareSync(password, user.pass)) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+          username: user.username,
+        },
+        JWT_SECRET
+      );
+      localStorage.setItem("user", user.username);
+      res.cookie("user", user.username);
+      alert("Login Successful !");
+      res.redirect("/home");
+    } else {
+      alert("Wrong Password");
+      res.redirect("/login");
+    }
+  } else {
+    alert("User not registered");
+    res.redirect("/");
+  }
+};
+
+const getHome =  (req, res) => {
+    res.send("Home")
+};
+
+module.exports = { getRegister, postRegister, getLogin, postLogin, getHome};
--- 02.LabTwoAndThree/SimpleAuth/routes/userRoutes.js
+++ 02.LabTwoAndThree/SimpleAuth/routes/userRoutes.js
@@ -1,112 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-const alert = require("alert");
-const jwt = require("jsonwebtoken");
-var LocalStorage = require("node-localstorage").LocalStorage,
-localStorage = new LocalStorage("./scratch");
 
-const User = require("../model/usersModel/user");
 const isLoggedIn = require("../middleWare/authMiddleWare");
+const { getRegister, postRegister, getLogin, postLogin, getHome} = require("../Controller/authcontroller");
 
-router.use("/home",isLoggedIn);
+router.use("/home", isLoggedIn);
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-JWT_SECRET = "aghddshjhdhebgdshdjkhxsnakjdsjsjdd674773$%$#sshsshhsdhssj";
+router.use("/home", isLoggedIn);
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-router.get("/", (req,res) =>{
-    res.send("Landing");
-})
-
-router.get("/register", (req, res) => {
-  res.sendFile("register.html", { root: "./views/authView" });
+router.get("/", (req, res) => {
+  res.send("Landing");
 });
+router.route("/register").get(getRegister).post(postRegister);
 
-router.post("/register", async (req, res) => {
-    const { username, email, gender, password, Confirm_password } = req.body;
+router.route("/login").get(getLogin).post(postLogin);
 
-    if (!username || typeof username !== "string") {
-      alert("Inavlid Username");
-      res.redirect("/register");
-    }
-    if (!email || typeof email !== "string") {
-      alert("Inavlid email");
-      res.redirect("/register");
-    }
-  
-    if (password.length >= 6) {
-      if (password === Confirm_password) {
-        const pass = await bcrypt.hash(password, 10);
-  
-        try {
-          const newUser = await User.create({
-            username,
-            email,
-            gender,
-            pass,
-          });
-          console.log("User created successfully", newUser);
-          res.redirect("/login");
-        } catch (error) {
-          if (error.code === 11000) {
-            alert("Username or Email already registered");
-          }
-          console.log(error);
-        }
-      } else {
-        alert("Password doesn't match !");
-        res.redirect("/register");
-      }
-    } else {
-      alert("Password must be at least 6 digits");
-      res.redirect("/register");
-    }
-});
+router.get("/home", getHome);
 
-router.get("/login", (req, res) => {
-  res.sendFile("login.html", { root: "./views/authView" });
-});
-
-router.post("/login", async (req, res) => {
-    const { username, email, password } = req.body;
-
-    const user = await User.findOne({ email });
-  
-    if (user) {
-      if (bcrypt.compareSync(password, user.pass)) {
-
-        const token = jwt.sign(
-            {
-              id: user._id,
-              username: user.username,
-            },
-            JWT_SECRET
-          );
-        localStorage.setItem("token",token)
-        localStorage.setItem("user", user.username);
-        res.cookie("user", user.username);
-        alert("Login Successful !");
-        res.redirect("/home");
-      } else {
-        alert("Wrong Password");
-        res.redirect("/login");
-      }
-    } else {
-      alert("User not registered");
-      res.redirect("/register");
-    }
-});
-
-router.get("/home",  (req, res) => {
-    res.send("Home")
-});
-
-router.use( (req, res) => {
-    res.status(401).send("404! Page doesn't exist");
+router.use((req, res) => {
+  res.status(401).send("404! Page doesn't exist");
 });
 
 module.exports = router;
