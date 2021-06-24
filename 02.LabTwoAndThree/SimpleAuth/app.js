aa93a22adc5d119c302b6671fdeb1dd184d0fa68
Sananda <sananda@iut-dhaka.edu>
2021-06-24 16:10:36 +0600

    database connected and model created

--- 02.LabTwoAndThree/SimpleAuth/app.js
+++ 02.LabTwoAndThree/SimpleAuth/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 
 const userRouter = require("./routes/userRoutes");
+
+mongoose
+  .connect(process.env.Mongo_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((res) => {
+    console.log("DB Connected!");
+  })
+  .catch((err) => {
+    console.log(Error, err.message);
+  });
 
 app.use(userRouter);
 
